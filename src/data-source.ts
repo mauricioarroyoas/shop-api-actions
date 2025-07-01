6dad4cd579be59f1e9296ca9617be0d73c27e9ca
mauricioarrequena <mauricio.ar.requena@gmail.com>
2025-07-01 05:03:54 -0400

    feat: added new deploymet config

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,19 +4,19 @@ import { DataSource } from "typeorm";
 import { Product } from "./entities/Product";
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "mydblocal",
-
   // type: "postgres",
-  // host: process.env.PGHOST,
-  // port: parseInt(process.env.PGPORT || "5432"),
-  // username: process.env.PGUSER,
-  // password: process.env.PGPASSWORD,
-  // database: process.env.PGDATABASE,
+  // host: "localhost",
+  // port: 5432,
+  // username: "postgres",
+  // password: "postgres",
+  // database: "mydblocal",
+
+  type: "postgres",
+  host: process.env.PGHOST,
+  port: parseInt(process.env.PGPORT || "5432"),
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
 
   synchronize: true,
   logging: false,
